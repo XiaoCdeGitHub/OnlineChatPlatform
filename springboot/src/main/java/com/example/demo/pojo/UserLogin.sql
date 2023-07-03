c3cce895bdbe5ba2f6f178f4cfc5a0553eaa582c
cui <1692338302@qq.com>
2023-07-03 23:13:26 +0800

    delete

--- springboot/src/main/java/com/example/demo/pojo/UserLogin.sql
+++
@@ -1,7 +0,0 @@
--- auto Generated on 2023-06-27
--- DROP TABLE IF EXISTS user_login;
-CREATE TABLE user_login(
-	email VARCHAR (50) NOT NULL COMMENT 'email',
-	`password` VARCHAR (50) NOT NULL DEFAULT '' COMMENT 'password',
-	PRIMARY KEY (email)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT 'user_login';
--- springboot/src/main/java/com/example/demo/pojo/UserRelate.sql
+++
@@ -1,9 +0,0 @@
--- auto Generated on 2023-06-29
--- DROP TABLE IF EXISTS user_relate;
-CREATE TABLE user_relate(
-	sendUserId VARCHAR (50) NOT NULL COMMENT 'sendUserId',
-	receiveUserId VARCHAR (50) NOT NULL DEFAULT '' COMMENT 'receiveUserId',
-	lastTime VARCHAR (50) NOT NULL DEFAULT '' COMMENT 'lastTime',
-	lastWord VARCHAR (50) NOT NULL DEFAULT '' COMMENT 'lastWord',
-	PRIMARY KEY (sendUserId)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT 'user_relate';
